87dac18707fe2693ac82876000801d03e13c5d0e
Sarah Lewis <sarah@yokoco.com>
2020-06-18 09:30:27 -0700

    Add license details

--- wp-sanitization-demo.php
+++ wp-sanitization-demo.php
@@ -6,6 +6,21 @@
  * Author URI:      https://www.yokoco.com/
  * Text Domain:     yoko-wp-sanitization-demo
  * Version:         0.1
+ * License:         GPL3
+ *
+ * WP Sanitization Demo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * WP Sanitization Demo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WP Sanitization Demo. If not, see
+ * https://www.gnu.org/licenses/gpl-3.0.html.
  *
  * @package         Yoko_WP_Sanitization_Demo
  */
